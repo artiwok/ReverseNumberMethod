54731200a021338598e2c8955c204381d249b782
artiwok <artemolga@ukr.net>
2020-11-03 21:03:19 +0200

    Add project files.

---
+++ ReverseNumberMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseNumber();
+        }
+
+        static void ReverseNumber()
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string s = Console.ReadLine();
+                Console.Write("The reverse number is: ");
+                char[] num = s.ToCharArray();
+
+                for (int i = num.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(num[i]);
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
